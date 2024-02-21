039ab31b1b8ed47b6cdbc634a5676083c8e5941b
joropge <jorgeop1997@gmail.com>
2024-02-21 09:44:42 +0100

    js realizado a falta de pruebas

---
+++ build/js/app.js
@@ -0,0 +1,91 @@
+document.addEvenListenner("DOMContentLoaded", function() {
+    iniciarApp();
+});
+
+function iniciarApp() {
+    navegacionFija();
+
+    crearGaleria();
+
+    scrollNav();
+}
+
+function navegacionFija() {
+    const barra = document.querySelector(".header");
+    const video = document.querySelector(".video");
+    const body = document.querySelector("body");
+
+    window.addEventListener("scroll", function() {
+        if (video.getBoundingClientRect().bottom < 0 && this.window.innerWidth >= 468) {
+            barra.classList.add("fijo");
+            body.classList.add("body-scroll");
+        } else {
+            barra.classList.remove("fijo");
+            body.classList.remove("body-scroll");
+        }
+    });
+}
+
+function scrollNav() {
+    const enlaces = document.querySelectorAll(".nave-principal a");
+
+    enlaces.forEach((enlace) => {
+        enlace.addEventListener("click", function(e) {
+            e.preventDefault();
+
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({ behavior: "smooth" });
+        });
+    });
+}
+
+function crearGaleria() {
+    const galeria = document.querySelector(".galeriaImgs");
+
+    for (let i = 1; i <= 6; i++) {
+        const imagen = document.createElement("picture");
+        imagen.innerHTML = `
+  <source srcset="build/img/thumb/${i}.png" type="image/png">
+  <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galeria">`;
+        imagen.onclick = function () {
+            mostrarImagen(i);
+        };
+
+        galeria.appendChild(imagen);
+    }
+}
+
+function mostrarImagen(id) {
+    const imagen = document.createElement("picture");
+    imagen.innerHTML = `
+  <source srcset="build/img/big/${id}.png" type="image/png">
+  <img loading="lazy" width="200" height="300" src="build/img/big/${id}.jpg" alt="imagen galeria">
+      `;
+
+    // Crea el Overlay con la imagen
+    const overlay = document.createElement("DIV");
+    overlay.appendChild(imagen);
+    overlay.classList.add("overlay");
+    overlay.onclick = function () {
+        const body = document.querySelector("body");
+        body.classList.remove("fijar-body");
+        overlay.remove();
+    };
+
+    // Boton para cerrar el Modal
+    const cerrarModal = document.createElement("P");
+    cerrarModal.textContent = "X";
+    cerrarModal.classList.add("btn-cerrar");
+    cerrarModal.onclick = function () {
+        const body = document.querySelector("body");
+        body.classList.remove("fijar-body");
+        overlay.remove();
+    };
+    overlay.appendChild(cerrarModal);
+
+    // Añadirlo al HTML
+    const body = document.querySelector("body");
+    body.appendChild(overlay);
+    body.classList.add("fijar-body");
+}
